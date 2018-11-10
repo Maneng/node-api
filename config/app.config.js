4f95eb842070ef044ea217368a0a5b6b4acddd97
maneng <marnner@163.com>
2018-11-10 13:52:47 +0800

    update

--- config/app.config.js
+++ config/app.config.js
@@ -1,7 +1,7 @@
 const argv = require('yargs').argv;
 
 exports.MONGODB = {
-	uri: `mongodb://127.0.0.1:${argv.dbport || '27017'}/NodePress`,
+	uri: `mongodb://47.97.102.250:${argv.dbport || '27017'}/NodePress`,
 	username: argv.db_username || 'DB_username',
 	password: argv.db_password || 'DB_password'
 };
